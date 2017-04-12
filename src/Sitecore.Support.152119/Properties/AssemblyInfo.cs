3525ed8dc1d46c46fa82fedc33b04f6641afe128
DK\psk <psk@sitecore.net>
2017-04-12 13:01:42 -0400

    Fixed using configuration

---
+++ src/Sitecore.Support.152119/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.152119")]
+[assembly: AssemblyProduct("Sitecore.Support.152119")]
+[assembly: ComVisible(false)]
